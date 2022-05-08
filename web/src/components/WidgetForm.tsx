76f3e487bf0cd1a77981b25d19e6b2bd3de8f76a
devRMA <rafael271alves@gmail.com>
2022-05-08 16:41:23 -0300

    created types of feedbacks

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feedbacksTypes = {
+    bug: {
+        title: "Problema",
+        image: {
+            source: bugImageUrl,
+            alt: "Imagem de um inseto",
+        },
+    },
+    idea: {
+        title: "Ideia",
+        image: {
+            source: ideaImageUrl,
+            alt: "Imagem de uma lâmpada",
+        },
+    },
+    other: {
+        title: "Outro",
+        image: {
+            source: thoughtImageUrl,
+            alt: "Imagem de um balão de pensamento",
+        },
+    },
+};
+
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,7 +36,23 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
-            <p>Hello World</p>
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbacksTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            type="button"
+                        >
+                            <img
+                                src={value.image.source}
+                                alt={value.image.alt}
+                            />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
 
             <footer className="text-xl text-neutral-400">
                 Feito com ❤ por{" "}
