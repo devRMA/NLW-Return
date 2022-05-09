6cabaa50c1a82a22d0eae242678410026bb0b3c8
Rafael <rafael271alves@gmail.com>
2022-05-09 12:17:28 -0300

    added screenshot preview and save screenshot in state

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,11 +1,19 @@
 import html2canvas from "html2canvas";
 
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
 
 import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (screenshot: string | null) => void;
+}
+
+export function ScreenshotButton({
+    screenshot,
+    onScreenshotTook,
+}: ScreenshotButtonProps) {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
 
     async function handleTakeScreenshot() {
@@ -13,9 +21,25 @@ export function ScreenshotButton() {
         const canvas = await html2canvas(document.documentElement);
         const base64image = canvas.toDataURL("image/png");
 
-        console.log(base64image);
-
+        onScreenshotTook(base64image);
         setIsTakingScreenshot(false);
+    }
+
+    if (screenshot) {
+        return (
+            <button
+                type="button"
+                className="p-1 w-10 h-10 rounded-md flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                onClick={() => onScreenshotTook(null)}
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    backgroundPosition: "right bottom",
+                    backgroundSize: 180,
+                }}
+            >
+                <Trash weight="fill" />
+            </button>
+        );
     }
 
     return (
