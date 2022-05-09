2224fa9db71836092abec04483a4069e64947adb
Rafael <rafael271alves@gmail.com>
2022-05-09 16:58:32 -0300

    removed repeated class

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -57,7 +57,7 @@ export function FeedbackContentStep({
 
             <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
-                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-600 dark:placeholder-zinc-400 text-zinc-800 dark:text-zinc-100 border-zinc-400 dark:border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-200 dark:scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-600 dark:placeholder-zinc-400 text-zinc-800 dark:text-zinc-100 border-zinc-400 dark:border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-200 dark:scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
                     onChange={(e) => setComment(e.target.value)}
                     value={comment}
