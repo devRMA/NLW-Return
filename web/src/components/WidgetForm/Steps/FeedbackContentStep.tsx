1036950b2da9c919486cfd1984c1849065dc7bfd
Rafael <rafael271alves@gmail.com>
2022-05-09 13:28:28 -0300

    deactivating the button if there is no message

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -66,8 +66,9 @@ export function FeedbackContentStep({
                     />
 
                     <button
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
                         type="submit"
+                        disabled={comment.length === 0}
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-brand-500"
                     >
                         Enviar feedback
                     </button>
