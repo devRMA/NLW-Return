46192c4b7a94ba43e786a00a7bc568657aaaf2c8
Rafael <rafael271alves@gmail.com>
2022-05-09 16:29:52 -0300

    added weight to icons

--- web/src/components/ThemeToggle.tsx
+++ web/src/components/ThemeToggle.tsx
@@ -21,9 +21,9 @@ export function ThemeToggle() {
             className="my-auto w-8 h-8 mx-2 flex justify-center items-center rounded-lg focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-zinc-900 focus:ring-brand-500 bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-700 dark:hover:bg-zinc-600"
         >
             {isDark ? (
-                <Sun className="w-6 h-6" />
+                <Sun weight="fill" className="w-6 h-6" />
             ) : (
-                <MoonStars className="w-6 h-6" />
+                <MoonStars weight="fill" className="w-6 h-6" />
             )}
         </button>
     );
