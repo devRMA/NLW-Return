87ef1b99edf7232b0f83fe36cf8eddc35c2780bc
Rafael <rafael271alves@gmail.com>
2022-05-09 09:48:53 -0300

    added state to control selected feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugImageUrl from "../assets/bug.svg";
@@ -28,7 +30,11 @@ const feedbacksTypes = {
     },
 };
 
+type FeedbackType = keyof typeof feedbacksTypes;
+
 export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -36,23 +42,30 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-                {Object.entries(feedbacksTypes).map(([key, value]) => {
-                    return (
-                        <button
-                            key={key}
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            type="button"
-                        >
-                            <img
-                                src={value.image.source}
-                                alt={value.image.alt}
-                            />
-                            <span>{value.title}</span>
-                        </button>
-                    );
-                })}
-            </div>
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
+                    {Object.entries(feedbacksTypes).map(([key, value]) => {
+                        return (
+                            <button
+                                key={key}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                type="button"
+                                onClick={() =>
+                                    setFeedbackType(key as FeedbackType)
+                                }
+                            >
+                                <img
+                                    src={value.image.source}
+                                    alt={value.image.alt}
+                                />
+                                <span>{value.title}</span>
+                            </button>
+                        );
+                    })}
+                </div>
+            ) : (
+                <p>Feedback selecionado: {feedbackType}</p>
+            )}
 
             <footer className="text-xl text-neutral-400">
                 Feito com ❤ por{" "}
