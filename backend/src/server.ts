a7968ba2f215d9137a394a5527257ed2e0c369bc
Rafael <rafael271alves@gmail.com>
2022-05-10 09:48:44 -0300

    route created and json middleware added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/feedbacks", (req, res) => {
+    console.log(req.body);
+    
+    return res.send("Hello World");
+});
+
 app.listen(3333, () => {
     console.log("HTTP Server started on port 3333");
 });
