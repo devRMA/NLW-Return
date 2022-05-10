bdf4a4c758fe09f5fc4674174ac58085b55b9af9
Rafael <rafael271alves@gmail.com>
2022-05-10 10:40:44 -0300

    send email when new feedbacks are registered

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import nodemailer from "nodemailer";
+
 import { prisma } from "./prisma";
+
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 2525,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+});
 
 app.post("/feedbacks", async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +27,18 @@ app.post("/feedbacks", async (req, res) => {
             comment,
             screenshot,
         },
+    });
+
+    await transport.sendMail({
+        from: process.env.EMAIL_FROM,
+        to: process.env.EMAIL_TO,
+        subject: "Novo Feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+            `<p>Tipo do Feedback ${type}</p>`,
+            `<p>Comentário ${comment}</p>`,
+            `</div>`,
+        ].join("\n"),
     });
 
     return res.status(201).json({
